0e22a8f718f93eb7d44d7440ae3670d715088fa0
anky894 <ko282ko82@gmail.com>
2025-11-29 23:48:16 +0530

    avalon: Enable supported touch features
    Change-Id: Ief688f7f6e9b685cc297a8b6c8d94db5feef0df6

--- device.mk
+++ device.mk
@@ -47,7 +47,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Touch features
-$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_HTPR,false)
+$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
 
 # Vibrator
 PRODUCT_PACKAGES += \
