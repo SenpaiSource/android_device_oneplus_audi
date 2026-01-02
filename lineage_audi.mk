0214bccc1d004276b0b59524df7060dd467d06ae
anky894 <ko282ko82@gmail.com>
2026-01-02 21:54:19 +0530

    audi: Update from COS 11.F.12
    Change-Id: I42a83fc31f6276bdf4707f201ebe9d4bfe9cd25b

--- lineage_audi.mk
+++ lineage_audi.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := PJF110
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 16 BP2A.250605.015 1763201356522 release-keys" \
-    BuildFingerprint=OnePlus/PJF110/OP5CFBL1:16/UKQ1.231108.001/U.23f7937-2f2dd9-2fb722:user/release-keys \
+    BuildDesc="qssi-user 16 BP2A.250605.015 1766282259482 release-keys" \
+    BuildFingerprint=OnePlus/PJF110/OP5CFBL1:16/UKQ1.231108.001/U.2f3586c-dab26b-dbd0c4:user/release-keys \
     DeviceName=OP5CFBL1 \
     DeviceProduct=PJF110 \
     SystemDevice=OP5CFBL1 \
