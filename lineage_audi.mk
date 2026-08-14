bdf40eb89c9071f130d58fbe65fea8816bd56874
SATHIYA NARAYANAN T <25bai042@hicas.ac.in>
2026-08-14 17:44:41 +0530

    Enable reality engine

--- lineage_audi.mk
+++ lineage_audi.mk
@@ -34,6 +34,7 @@ TARGET_DISABLE_MATLOG := true
 HBM_SUPPORTED := true
 HBM_NODE := /sys/class/backlight/panel0-backlight/hbm_mode
 BYPASS_CHARGE_SUPPORTED := true
+USE_REALITY_ENGINE := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="qssi-user 16 BP2A.250605.015 1785124924474 release-keys" \
